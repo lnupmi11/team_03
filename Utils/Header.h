fa6c906ac8c1499033f7f6e3fb5e7cd91cbd9182
OHopiak <vorikus@gmail.com>
2017-05-07 19:40:06 +0300

    small changes

--- Utils/Header.h
+++ Utils/Header.h
@@ -10,10 +10,10 @@
 
 #if defined(_WIN32) || defined(_WIN64)
 #include <windows.h>
-#define CLS system("cls");
+#define CLS do{system("cls");}while(0)
 #elif defined(__unix) || defined(__unix__) || defined(__APPLE__) || defined(__MACH__) || defined(__linux__) || defined(__FreeBSD__)
 #include <sys/ioctl.h>
-#define CLS system("clear");
+#define CLS do{system("clear");}while(0)
 #else
 #define CLS /**/
 #endif
